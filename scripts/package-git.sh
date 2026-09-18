0f6e396a03dad83ab9660e7711f8519d86cd9b6c
icyleaf <icyleaf.cn@gmail.com>
2026-09-18 15:39:43 +0900

    refactor(packaging): inline omawarden-git PKGBUILD into workflow and build scripts (#172)

--- scripts/package-git.sh
+++ scripts/package-git.sh
@@ -33,12 +33,50 @@ BUILD_DIR="${ROOT_DIR}/dist/pkg-git"
 rm -rf "${BUILD_DIR}"
 mkdir -p "${BUILD_DIR}"
 
-cp "${ROOT_DIR}/dist/aur-git/PKGBUILD" "${BUILD_DIR}/"
-
+GIT_REPO_URL="https://github.com/icyleaf/omarchy-bitwarden.git"
 if [ "${USE_LOCAL}" = true ]; then
   echo "==> Using local repository for git source..."
-  sed -i "s|https://github.com/icyleaf/omarchy-bitwarden.git|file://${ROOT_DIR}|" "${BUILD_DIR}/PKGBUILD"
+  GIT_REPO_URL="file://${ROOT_DIR}"
 fi
+
+cat << PKG_EOF > "${BUILD_DIR}/PKGBUILD"
+# Maintainer: icyleaf <icyleaf.cn at gmail dot com>
+
+pkgname=omawarden-git
+pkgver=0.8.0.r0.g0000000
+pkgrel=1
+pkgdesc="High-performance Bitwarden CLI and resident daemon (VCS build from develop branch)"
+arch=('x86_64' 'aarch64')
+url="https://github.com/icyleaf/omarchy-bitwarden"
+license=('MIT')
+depends=('libsecret' 'wl-clipboard')
+makedepends=('cargo' 'git')
+optdepends=('libfido2: WebAuthn / Passkey / FIDO2 security key support')
+provides=('omawarden' 'omawarden-bin')
+conflicts=('omawarden' 'omawarden-bin')
+options=('!lto')
+source=("omarchy-bitwarden::git+${GIT_REPO_URL}#branch=develop")
+sha256sums=('SKIP')
+
+pkgver() {
+    cd "\${srcdir}/omarchy-bitwarden"
+    _ver=\$(grep '^version = ' omawarden/Cargo.toml | head -n 1 | cut -d'"' -f2 | cut -d'-' -f1)
+    _rev=\$(git rev-list --count HEAD)
+    _hash=\$(git rev-parse --short=7 HEAD)
+    printf "%s.r%s.g%s" "\$_ver" "\$_rev" "\$_hash"
+}
+
+build() {
+    cd "\${srcdir}/omarchy-bitwarden/omawarden"
+    cargo build --frozen --release --bin omawarden
+}
+
+package() {
+    cd "\${srcdir}/omarchy-bitwarden"
+    install -Dm755 omawarden/target/release/omawarden "\${pkgdir}/usr/bin/omawarden"
+    install -Dm644 LICENSE "\${pkgdir}/usr/share/licenses/\${pkgname}/LICENSE"
+}
+PKG_EOF
 
 echo "==> Packaging omawarden-git via makepkg..."
 (cd "${BUILD_DIR}" && makepkg -f --nodeps)
